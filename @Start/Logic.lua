771c196b9a1550fbb0042f75758c4519273f39bd
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-12-10 15:57:11 +0800

    Core4 Compatability

--- @Start/Logic.lua
+++ @Start/Logic.lua
@@ -13,7 +13,7 @@ function check()
         SKIN:Bang('!UpdateMeasure', 'JaxCoreYes')
     else
         print('Update required - '..ParsedVer..'>='..CoreVer)
-        SKIN:Bang('!WriteKeyValue', 'Data', 'DownloadLink', 'https://github.com/EnhancedJax/-JaxCore/releases/download/v'..ParsedVerFull..'/JaxCore_v'..ParsedVerFull..'.rmskin', SavePos)
+        SKIN:Bang('!WriteKeyValue', 'Data', 'DownloadLink', 'https://github.com/Jax-Core/JaxCore/releases/download/v'..ParsedVerFull..'/JaxCore_v'..ParsedVerFull..'.rmskin', SavePos)
         SKIN:Bang('!WriteKeyValue', 'Data', 'SaveLocation', SaveLocation, SavePos)
         SKIN:Bang('!UpdateMeasure', 'JaxCoreNo')
     end
