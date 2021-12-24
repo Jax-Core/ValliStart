bc004d411654eb0970424902af12dd809189e797
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-12-24 22:26:57 +0800

    Revert "v3.0 added position options"
    This reverts commit 00b9ce5250f6ee8bcc33bacb694df87bba2e0f79.

--- @Resources/Actions/Source code/ValliStart.ahk
+++ @Resources/Actions/Source code/ValliStart.ahk
@@ -2,7 +2,6 @@
 #NoTrayIcon
 IniRead, OutputVar, Hotkeys.ini, Variables, Key
 IniRead, Variable, ..\Vars.inc, Variables, ReplaceWin
-IniRead, Position, ..\Vars.inc, Variables, Position
 IniRead, RainmeterPath, Hotkeys.ini, Variables, RMPATH
 
 if (Variable = 0)
@@ -18,12 +17,7 @@ else
 }
 
 Button:
-    if (Position = "MousePosition")
-    {
-        MouseGetPos, xpos, ypos 
-        Run "%RainmeterPath%" [!CommandMeasure Func valliMoveMouse(%xpos%`,%ypos%) ValliStart\Main]
-    }
-    Run "%RainmeterPath%" [!UpdateMeasure mToggle ValliStart\Main]
+    Run "%RainmeterPath% "!UpdateMeasure "mToggle" "ValliStart\Main" "
 Return
 
 DisabledWin:
--- @Resources/Lua/Func.lua
+++ @Resources/Lua/Func.lua
@@ -1,7 +1,4 @@
 function Initialize()
-
-    -- --------- change divider visibility and music measure disabilitiy -------- --
-
     local ModuleArray = {}
     local function has_value(tab, val)
         for index, value in ipairs(tab) do
@@ -21,52 +18,5 @@ function Initialize()
     if not has_value(ModuleArray, 'Music') then
         SKIN:Bang('[!DisableMeasureGroup Music][!UpdateMeasureGroup Music]')
         print('Disabled all music measures')
-    end
-
-    -- ------------------------------- positioning ------------------------------ --
-    
-    local pos = SKIN:GetVariable('Position')
-    if pos == 'Custom' then
-        SKIN:Bang('!Draggable', '1')
-    elseif pos == 'MousePosition' then
-        SKIN:Bang('[!SetWindowPosition 50% 50% 50% 50%]')
-    else
-        local posX = string.sub(pos, 2, 2)
-        local posY = string.sub(pos, 1, 1)
-        local pad = SKIN:GetVariable('ScreenPadding')
-        local saw = SKIN:GetVariable('SCREENAREAWIDTH')
-        local sah = SKIN:GetVariable('SCREENAREAHEIGHT')
-        local moveX = 0
-        local moveY = 0
-        local anchorX = 0
-        local anchorY = 0
-        
-        if posX == 'L' then moveX = pad
-        elseif posX == 'C' then
-            moveX = (saw/2)
-            anchorX = "50%"
-        elseif posX == 'R' then
-            moveX = (saw-pad)
-            anchorX = "100%"
-        end
-        
-        if posY == 'T' then moveY = pad
-        elseif posY == 'C' then
-            moveY = (sah/2)
-            anchorY = "50%"
-        elseif posY == 'B' then
-            moveY = (sah-pad)
-            anchorY = "100%"
-        end
-
-        SKIN:Bang('!SetWindowPosition '..moveX..' '..moveY..' '..anchorX..' '..anchorY)
-    end
-    SKIN:Bang('[!CommandMeasure Func valliMoveMouse(1,1) ValliStart\\Main]')
-end
-
-function valliMoveMouse(xpos, ypos)
-    local isOff = SKIN:GetMeasure('mToggle'):GetValue()
-    if isOff ~= 0 then
-        SKIN:Bang('[!SetWindowPosition '..xpos..' '..ypos..' 50% 50%]')
     end
 end
